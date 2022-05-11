aa16595dee8f68e2167af507038d30d5b134ee4b
Яковлева Ольга Викторовна <OVYakovleva@alfabank.ru>
2022-05-11 21:09:30 +0300

    исправлен CORS

--- api.py
+++ api.py
@@ -1,28 +1,36 @@
 import model
 from flask import Flask, request, jsonify
 
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # список всех фильмов
 @app.route('/api/movies', methods = ['GET'])
+@cross_origin()
 def get_all_movies():
     movies = model.get_all_movies()
     return jsonify(movies)
 
 # список новых фильмов (лаб. 1.2)
 @app.route('/api/movies/new', methods = ['GET'])
+@cross_origin()
 def get_new_movies():
     movies = model.get_new_movies()
     return jsonify(movies)
 
 # фильм по id
 @app.route('/api/movies/<id>', methods = ['GET'])
+@cross_origin()
 def get_movie(id):
     movie = model.get_movie(id)
     return jsonify(movie)
 
 # оценки (фильтруем по userId и movieId)
 @app.route('/api/ratings', methods=['GET'])
+@cross_origin()
 def get_ratings():
     args = request.args
     ratings = model.get_ratings(args.to_dict())
@@ -30,6 +38,7 @@ def get_ratings():
 
 # новая оценка
 @app.route('/api/ratings', methods=['POST'])
+@cross_origin()
 def post_rating():
     rating = request.get_json(force=True)
     model.add_rating(rating)
@@ -37,6 +46,7 @@ def post_rating():
 
 # теги (фильтруем по userId и movieId)
 @app.route('/api/tags', methods=['GET'])
+@cross_origin()
 def get_tags():
     args = request.args
     tags = model.get_tags(args.to_dict())
@@ -44,6 +54,7 @@ def get_tags():
 
 # новый тег
 @app.route('/api/tags', methods=['POST'])
+@cross_origin()
 def post_tag():
     tag = request.get_json(force=True)
     model.add_tag(tag)
